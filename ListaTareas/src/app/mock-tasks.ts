1f107354ae95586e6278af61d209f154bea24715
Mikeltxo15 <inumikel@protonmail.com>
2022-09-03 16:18:27 -0300

    ListadeItems

--- ListaTareas/src/app/mock-tasks.ts
+++ ListaTareas/src/app/mock-tasks.ts
@@ -3,19 +3,19 @@ import { Task } from "./Tasks"
 export const TASKS:Task[] = [
     {
         id: 1,
-        text: "1",
+        text: "a",
         day: "1",
         reminder: true
     },
     {
         id: 2,
-        text: "2",
+        text: "b",
         day: "2",
         reminder: true
     },
     {
         id: 3,
-        text: "3",
+        text: "c",
         day: "3",
         reminder: true
     },
