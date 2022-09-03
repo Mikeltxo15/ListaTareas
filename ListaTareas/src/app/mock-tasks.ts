07152457f77e75814a595bad77077c5b41ae7aa0
Mikeltxo15 <inumikel@protonmail.com>
2022-09-03 15:36:45 -0300

    Mock agregado

---
+++ ListaTareas/src/app/mock-tasks.ts
@@ -0,0 +1,20 @@
+export const TASKS = [
+    {
+        id: 1,
+        text: "1",
+        day: "1",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "2",
+        day: "2",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "3",
+        day: "3",
+        reminder: true
+    },
+]
